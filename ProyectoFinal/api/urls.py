0077f18d3915d1ec9aa4800f83db7cef712c7884
Gilda <gmrp22@hotmail.com>
2021-01-31 17:02:51 -0600

    Cambio url

--- ProyectoFinal/api/urls.py
+++ ProyectoFinal/api/urls.py
@@ -11,9 +11,9 @@ router.register(r'seller', viewsets.SellerViewSet)
 router.register(r'product', viewsets.ProductViewSet)
 router.register(r'sale', viewsets.SaleViewSet)
 router.register(r'purchase', viewsets.PurchaseViewSet)
-router.register(r'ownproducts', viewsets.OwnProductsViewSet)
-router.register(r'catalogue', viewsets.CatalogueViewSet)
-router.register(r'salereport', viewsets.SaleReport)
+router.register(r'ownproducts', viewsets.OwnProductsViewSet, basename='ownproducts')
+router.register(r'catalogue', viewsets.CatalogueViewSet, basename='catalogue')
+router.register(r'salereport', viewsets.SaleReport, basename='salereport')
 urlpatterns = [
     path('api/', include(router.urls)),
     url(r"^api/token", obtain_auth_token, name="api-token"),
